815b0e4de7161af2a7b3187fa3ced3f6984a5d09
Aditya Kamodiya <adityakamodiya@gmail.com>
2024-10-10 15:19:20 +0530

    commit

--- connection.js
+++ connection.js
@@ -1,4 +1,6 @@
 import{MongoClient} from "mongodb"
-const connection = MongoClient.connect("mongodb://127.0.0.1:27017")
+const connection = MongoClient.connect("mongodb+srv://adityakamodiya:11223344@cluster0.j4ukslx.mongodb.net/Aadii_infotech?retryWrites=true&w=majority&appName=Cluster0")
 export const dbName = "Aadii_infotech"
 export default connection
+// mongodb://127.0.0.1:27017
+// mongodb+srv://adityakamodiya:11223344@cluster0.j4ukslx.mongodb.net/MulterData?retryWrites=true&w=majority&appName=Cluster0
